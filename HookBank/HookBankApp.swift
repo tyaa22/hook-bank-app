a387bca56fbd6d45f3e772ac8947e0658f46a3ce
Cyintia <valeria_lupis@aleeas.com>
2026-08-18 14:37:30 +0700

    Initial Commit

---
+++ HookBank/HookBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  HookBankApp.swift
+//  HookBank
+//
+//  Created by Cyintia Limmanto on 18/08/26.
+//
+
+import SwiftUI
+
+@main
+struct HookBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
